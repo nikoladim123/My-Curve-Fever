cee87269e86800e89ad54ac6f48a2e9d309c3b09
nikoladim123 <nikoladimitrijevic1991@gmail.com>
2018-10-02 00:55:37 +0200

    asd

--- asdasd.js
+++ asdasd.js
@@ -1,25 +1,28 @@
-var RAF = setInterval(update, 1000 / 60);
 
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
+var canvasTwo = document.getElementById("canvasTwo");
+var ctxTwo = canvasTwo.getContext("2d");
+
+window.addEventListener("keydown", function(e) {
+  keys[e.keyCode] = true
+});
+window.addEventListener("keyup", function(e) {
+  keys[e.keyCode] = false
+});
+
 var w = canvas.width
 var h = canvas.height
 var keys = []
 
-window.addEventListener("keydown", function(e) {
-  keys[e.keyCode] = true
-})
-window.addEventListener("keyup", function(e) {
-  keys[e.keyCode] = false
-})
 
 var player = {
   x: w / 2, //player position X
   y: h / 2, //player position Y
-  r: 3,
+  r: 7,
   a: 0,
   t: 3 * (Math.PI / 180),
-  s: 1.5,
+  s: 1,
   c: "#8E35EF",
   h: 250, //isprekidana linija
   ht: 1
@@ -32,7 +35,7 @@ function getCIDCol(ctx) {
 
   var id = ctx.getImageData(next.x, next.y, 1, 1);
   //console.log(id);
-  console.log(id.data[1]);
+  console.log(id.data);
   return (id.data[1]);
 }
 
@@ -40,9 +43,11 @@ var next = {
   x: 0,
   y: 0
 };
-
+//interval for fps
+var myInterval = setInterval(update, 1000 / 60);
 //update function
 function update() {
+  ctxTwo.clearRect(0, 0, canvas.width, canvas.height);
 
   if (keys[39]) {
     player.a += player.t
@@ -55,7 +60,7 @@ function update() {
   player.y += Math.sin(player.a) * player.s
 
 
-  if (Math.floor(Math.random() * 100) == 1) {
+  if (Math.floor(Math.random() * player.h) == 1) {
     player.ht = setTimeout(function() {
       player.ht = 1;
     }, player.h)
@@ -64,32 +69,38 @@ function update() {
   // draw collision status
   if (getCIDCol(ctx)) {
     ctx.fillStyle = '#F00';
-    //clearInterval(RAF);
-
+    clearInterval(myInterval);
   } else ctx.fillStyle = '#0F0';
   ctx.fillRect(4, 4, 16, 16);
 
   if (getCIDCol(ctx) >= 51 && getCIDCol(ctx) <= 53) {
-    clearInterval(RAF);
+    clearInterval(myInterval);
   }
-
-  render()
+  render();
 }
 
-var image = new Image();
-image.src = 'https://42works.net/wp-content/uploads/2015/02/How-to-Create-Short-URLs-For-Your-Blog-Using-Google-Shortener.png';
-ctx.drawImage(image, 0, 0, 400, 400)
+
 
 function render() {
   if (player.ht == 1) {
-    ctx.beginPath()
-    ctx.arc(player.x, player.y, player.r, 1, Math.PI * 2, false)
-    ctx.fillStyle = player.c
-    //ctx.strokeStyle="yellow";
-    //ctx.stroke();
-    ctx.fill();
-    ctx.closePath()
+    ctx.save();
+    ctx.translate(player.x , player.y )
+    ctx.rotate(player.a);
+    ctx.fillStyle = player.c;
+    ctx.fillRect(-1, 0, player.r,player.r);
+    ctx.restore();
   }
+  var image = new Image();
   image.src = 'https://42works.net/wp-content/uploads/2015/02/How-to-Create-Short-URLs-For-Your-Blog-Using-Google-Shortener.png';
-  ctx.drawImage(image, player.x, player.y, 10, 10)
+
+
+ctxTwo.save();
+ctxTwo.translate(player.x, player.y);
+ctxTwo.rotate(player.a);
+ctxTwo.drawImage(image, -5 , 0, 10, 10);
+ctxTwo.restore();
+//ctxTwo.fillRect(20,20,20,20);
+//ctxTwo.fillStyle = '#0F0'
+
+        //ctxTwo.rotate(player.t);
 }
