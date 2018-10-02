7e896d03f91fcc6bbeacdeff1134dff967367836
nikoladim123 <nikoladimitrijevic1991@gmail.com>
2018-10-02 15:51:03 +0200

    m

--- asdasd.js
+++ asdasd.js
@@ -1,4 +1,3 @@
-
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var canvasTwo = document.getElementById("canvasTwo");
@@ -11,31 +10,37 @@ window.addEventListener("keyup", function(e) {
   keys[e.keyCode] = false
 });
 
-var w = canvas.width
-var h = canvas.height
+//seting canvas width as width of screen
+canvas.width = window.innerWidth;
+canvasTwo.width =window.innerWidth;
+canvas.height = window.innerHeight;
+canvasTwo.height =window.innerHeight;
+//canvas widht/height variables
+var w = canvas.width;
+var h = canvas.height;
 var keys = []
 
 
 var player = {
   x: w / 2, //player position X
   y: h / 2, //player position Y
-  r: 7,
-  a: 0,
-  t: 3 * (Math.PI / 180),
-  s: 1,
+  r: 10, //snake thicknes
+  a: 0, //angle in percent for starting angle, and changed on event listener
+  t: 3 * (Math.PI / 180), //calculate percent and adds to player.a (corner radius)
+  s: 2, //snake speed
   c: "#8E35EF",
-  h: 250, //isprekidana linija
+  h: 200, //duzina prekida na liniji
+  hc: 4, //sansa
   ht: 1
 }
 
 
 function getCIDCol(ctx) {
-  next.x = player.x + Math.cos(player.a) * (player.r + 1);
-  next.y = player.y + Math.sin(player.a) * (player.r + 1);
-
-  var id = ctx.getImageData(next.x, next.y, 1, 1);
+  next.x = player.x + Math.cos(player.a) * (player.r + 2);
+  next.y = player.y + Math.sin(player.a) * (player.r + 2);
+  var id = ctx.getImageData(next.x, next.y, 5, 5);
   //console.log(id);
-  console.log(id.data);
+  //console.log(id.data);
   return (id.data[1]);
 }
 
@@ -59,48 +64,51 @@ function update() {
   player.x += Math.cos(player.a) * player.s
   player.y += Math.sin(player.a) * player.s
 
-
-  if (Math.floor(Math.random() * player.h) == 1) {
+  //dashed line case
+  if (Math.floor(Math.random() * 1000) <= player.hc) {
     player.ht = setTimeout(function() {
       player.ht = 1;
     }, player.h)
   }
+  console.log(player.ht)
 
   // draw collision status
-  if (getCIDCol(ctx)) {
+  if (getCIDCol(ctx) && player.ht == 1) {
     ctx.fillStyle = '#F00';
     clearInterval(myInterval);
-  } else ctx.fillStyle = '#0F0';
+    console.log('draw collison stoped it')
+  } else {ctx.fillStyle = '#0F0';
   ctx.fillRect(4, 4, 16, 16);
+}
 
-  if (getCIDCol(ctx) >= 51 && getCIDCol(ctx) <= 53) {
+  if (getCIDCol(ctx) >= 51 && getCIDCol(ctx) <= 53 || next.x > w || next.x < 0 || next.y > h || next.y < 0) {
     clearInterval(myInterval);
+    console.log('stoped!!!');
   }
   render();
+  test++;
 }
 
 
-
+var test = 1;
 function render() {
   if (player.ht == 1) {
     ctx.save();
-    ctx.translate(player.x , player.y )
+    ctx.translate(player.x, player.y)
     ctx.rotate(player.a);
     ctx.fillStyle = player.c;
-    ctx.fillRect(-1, 0, player.r,player.r);
+    ctx.fillRect(0, -(player.r/2), player.r, player.r);
     ctx.restore();
   }
+
+
+
   var image = new Image();
   image.src = './custom.png';
+  ctxTwo.save();
+  ctxTwo.translate(player.x, player.y);
+  ctxTwo.rotate(player.a);
+  ctxTwo.drawImage(image, -7, -(player.r/2), player.r , player.r );
+  ctxTwo.restore();
 
-
-ctxTwo.save();
-ctxTwo.translate(player.x, player.y);
-ctxTwo.rotate(player.a);
-ctxTwo.drawImage(image, -7 , -3.5, 15, 15);
-ctxTwo.restore();
-//ctxTwo.fillRect(20,20,20,20);
-//ctxTwo.fillStyle = '#0F0'
-
-        //ctxTwo.rotate(player.t);
 }
